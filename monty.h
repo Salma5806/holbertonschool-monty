1f6fa7050a1821755f2370d8256628ce9de833bc
Salma5806 <5806@holbertonstudents.com>
2022-12-23 20:04:37 +0100

    prototype

--- monty.h
+++ monty.h
@@ -42,3 +42,6 @@ void pint(stack_t **stack, unsigned int line_number);
 void pop(stack_t **stack, unsigned int line_number);
 void swap(stack_t **stack, unsigned int line_number);
 void add(stack_t **stack, unsigned int line_number);
+void is_valid(char **token, stack_t **stack);
+void free_stack(stack_t **stack);
+#endif
