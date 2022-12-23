432e30ab9a2f07025c3a211a8325d6a49ced64a7
Salma5806 <5806@holbertonstudents.com>
2022-12-23 22:46:23 +0100

    monty

--- monty.h
+++ monty.h
@@ -37,7 +37,7 @@ typedef struct instruction_s
         char *opcode;
         void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
-void _push(stack_t **stack, unsigned int line_number);
+void _push(stack_t **stack, unsigned int n);
 void _pall(stack_t **stack, unsigned int line_number);
 void _pint(stack_t **stack, unsigned int line_number);
 void _pop(stack_t **stack, unsigned int line_number);
