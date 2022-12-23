62c8e952ce9a4e6b86a32091b3e515d4d26925a3
Salma5806 <5806@holbertonstudents.com>
2022-12-23 21:04:04 +0100

    func

--- anotherfunc.c
+++ anotherfunc.c
@@ -58,3 +58,20 @@ void swap(stack_t **stack, unsigned int line_number)
 	(*stack)->n = (*stack)->next->n;
 	(*stack)->next->n = temp;
 }
+/**
+ * _add - swaps the data in two nodes
+ * @stack: pointer to a stack_t
+ * @line_number: line number
+ */
+void _add(stack_t **stack, unsigned int line_number)
+{
+	if (stack == NULL || (*stack == NULL) || ((*stack)->next == NULL))
+	{
+		fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
+		if (*stack)
+			free_stack(stack);
+		exit(EXIT_FAILURE);
+	}
+	(*stack)->next->n = ((*stack)->next->n) + ((*stack)->n);
+	pop(stack, line_number);
+}
